30b4aca995c0317bb2b3ef663f56a77b48becb5b
AngusLin <angus848241995@gmail.com>
2016-06-05 20:16:33 +0800

    Player 1 with proper path
    modified Character.java to let player 1 run in proper path

--- src/sunsonfinalproject/Character.java
+++ src/sunsonfinalproject/Character.java
@@ -12,95 +12,125 @@ public class Character {
 	private MainApplet parent;
 	private PImage img;
 	private ArrayList<Character> targets = new ArrayList<Character>();
-	private int rightRadius, leftRadius, enterRightCircle, enterLeftCircle;
+	private int enterRightCircle, enterLeftCircle;
 	private int borderOffset = 0;
 	public int location=0, diff=0;
-	public float x, y;
-	public int winFlag=0;
-	public int bomb=0;
+	public float rightRadius, leftRadius, midX, midY;
+	public int winFlag = 0;
+	public int bomb = 0;
 	public int set_score=0;
-	private float angle1=PConstants.PI/2;
-	private float angle2=PConstants.PI/2;
 	private int playerIndex;
+	private float angle1=PConstants.PI * 3/2;
+	private float angle2=PConstants.PI * 3/2;
 	
 	/*
 	 * Store these variables when instance created.
 	 */
 	public Character(MainApplet parent, PImage image, int playerIndex){
 		this.parent = parent;
-		this.x = this.parent.appletLeftX;
-		this.y = this.parent.appletStartY + playerIndex * 50;
-		this.rightRadius = this.parent.appletCircleR + playerIndex * 50;
-		this.leftRadius = this.parent.appletCircleR + (3 - playerIndex) * 50;
-		this.enterRightCircle = this.enterLeftCircle = 0;
 		this.img = image;
+		this.img.resize(100, 100);
 		this.playerIndex=playerIndex;
+		this.enterRightCircle = this.enterLeftCircle = 0;
+		this.midX = this.parent.appletLeftX - img.width;
+		this.midY = (float) (this.parent.appletFirstPathStartY + playerIndex * 40 - 0.5 * this.img.height);
 	}
 	
 	public void playAgain(){
-		this.x = this.parent.appletLeftX;
-		this.y = this.parent.appletStartY + playerIndex * 50;
-		this.rightRadius = this.parent.appletCircleR + playerIndex * 50;
-		this.leftRadius = this.parent.appletCircleR + (3 - playerIndex) * 50;
+		
 		this.enterRightCircle = this.enterLeftCircle = 0;
 		this.winFlag=0;
 		this.bomb=0;
 		this.set_score=0;
 		this.angle1=PConstants.PI*3/2;
 		this.angle2=PConstants.PI*3/2;
+		
+		 
+		this.enterRightCircle = this.enterLeftCircle = 0;
 	}
 	
 	public void forward(){
 		if(winFlag == 2){} //win, don't run
-		else if(this.x < parent.appletRightX && this.y > parent.appletRightCircleCenterY && enterLeftCircle == 0){
+		else if(this.midX < parent.appletRightX && this.midY > parent.appletRightCircleCenterY && enterLeftCircle == 0){
 			//character in the below run path
-
-			this.x += 20;
-			if(this.x >= parent.appletRightX) this.x = parent.appletRightX;
-			System.out.println("下："+this.x + " " + this.y + " " + parent.appletRightX);
+			this.midX += 20;
+			if(this.midX >= parent.appletRightX) this.midX = parent.appletRightX;
+			System.out.println("下："+ this.midX + " " + this.midY);
 		}
-		else if(this.x >= parent.appletRightX && this.enterRightCircle < 2){
-			if(enterRightCircle == 0) enterRightCircle ++;
-			int cx = parent.appletRightX;
-			int cy = parent.appletRightCircleCenterY;
-			int r = 120;
-			this.x = (int)(cx + r*PApplet.cos(this.angle1));
-			this.y = (int)(cy + r*PApplet.sin(this.angle1));
-			this.angle1-=PConstants.PI/16;
-			if(x == cx && y < cy && enterRightCircle > 0){
+		else if(this.midX >= parent.appletRightX && this.enterRightCircle < 2){
+			if(enterRightCircle == 0){
+				this.rightRadius = (float) (this.midY - parent.appletRightCircleCenterY);
 				enterRightCircle ++;
 			}
-			System.out.println("下右中："+this.x + " " + this.y);
+			
+			midX = parent.appletRightX + this.rightRadius * PApplet.cos(this.angle1);
+			midY = parent.appletRightCircleCenterY - this.rightRadius * PApplet.sin(this.angle1);
+			this.angle1 += PConstants.PI/16;
+		
+			
+			if(midX <= parent.appletRightX && midY < parent.appletRightCircleCenterY && enterRightCircle > 0){
+				this.midX = parent.appletRightX;
+				System.out.println("End right circle: R " + this.rightRadius + " R' " + (parent.appletRightCircleCenterY - this.midY));
+				System.out.println("End right circle: A " + (this.angle1/PConstants.PI*180));
+				enterRightCircle ++;
+			}
+			System.out.println("下右中："+ (this.midX - img.width/2)  + " " + (this.midY - img.height/2));
 		}
-		else if(this.x > parent.appletLeftX && this.y < parent.appletRightCircleCenterY && this.y > parent.appletLeftCircleCenterY){
-			this.x -= 20;
-			System.out.println("中："+this.x + " " + this.y);
+		else if(this.midX > parent.appletLeftX && this.midY < parent.appletRightCircleCenterY && this.midY > parent.appletLeftCircleCenterY){
+			this.midX -= 20;
+			if(this.midX <= parent.appletLeftX) this.midX = parent.appletLeftX;
+			System.out.println("中："+ (this.midX - img.width/2)  + " " + (this.midY - img.height/2));
 		}
-		else if(this.x <= parent.appletLeftX && this.enterLeftCircle < 2){
-			if(enterLeftCircle == 0) enterLeftCircle ++;
-			int cx = parent.appletLeftX;
-			int cy = parent.appletLeftCircleCenterY;
-			int r = 120;
-			this.x = (int)(cx + r*PApplet.cos(this.angle2));
-			this.y = (int)(cy + r*PApplet.sin(this.angle2));
-			this.angle2+=PConstants.PI/16;
-			if(x == cx && y < cy && enterLeftCircle > 0){
+		else if(this.midX <= parent.appletLeftX && this.enterLeftCircle < 2){
+			if(enterLeftCircle == 0) {
+				this.leftRadius = this.midY - parent.appletLeftCircleCenterY;
 				enterLeftCircle ++;
 			}
-			System.out.println("中左上："+this.x + " " + this.y);
+			
+//			if(this.angle2 > )
+			
+			
+			
+			int cx = parent.appletLeftX;
+			int cy = parent.appletLeftCircleCenterY;
+			
+			this.midX = cx + this.leftRadius * PApplet.cos(this.angle2);
+			this.midY = cy - this.leftRadius * PApplet.sin(this.angle2);
+			this.angle2 -=PConstants.PI/16;
+			
+			if(midX >= cx && midY < cy && enterLeftCircle > 0){
+				this.midX = parent.appletLeftX;
+				enterLeftCircle ++;
+			}
+			System.out.println("中左上：" + (this.midX - img.width/2)  + " " + (this.midY - img.height/2));
 		}
-		else if(this.x < parent.appletRightX && this.y < parent.appletLeftCircleCenterY){
-			this.x += 20;
-			System.out.println("上："+this.x + " " + this.y);
+		else if(this.midX < parent.appletRightX && this.midY < parent.appletLeftCircleCenterY){
+			this.midX += 20;
+			System.out.println("上：" + (this.midX - img.width/2)  + " " + (this.midY - img.height/2));
 		}
-		else if(enterLeftCircle == 2 && this.x >= parent.appletRightX){
-			winFlag = 2;
+		else if(enterLeftCircle == 2 && this.midX >= parent.appletRightX){
+			this.midX = this.parent.appletLeftX - img.width;
+			this.midY = (float) (this.parent.appletFirstPathStartY - 0.5 * this.img.height); 
+			this.enterLeftCircle = this.enterRightCircle = 0;
+			this.angle1 = this.angle2 = PConstants.PI * 3 /2;
+			//winFlag = 2;
 		}
+		else{
+			System.out.println("沒跑道：" + (this.midX - img.width/2)  + " " + (this.midY - img.height/2));
+		}
+	}
+	
+	private double getAngle(){
+		double angle, sinValue;
+		sinValue = this.img.height / this.rightRadius; 
+		angle = Math.asin(sinValue);
+		System.out.println(angle / PConstants.PI);
+		return angle;
 	}
 	
 	public void end_play(){
 		this.img.resize(250, 250);
-		this.parent.image(this.img, x, y);
+		this.parent.image(this.img, midX - img.width/2, midY - img.height/2);
 	}
 	
 	/*
@@ -111,7 +141,9 @@ public class Character {
 		if(bomb == 1){
 			this.parent.tint(255,0,0);//red
 		}
-		this.parent.image(this.img, x, y);
+		//System.out.println("Display Char: " + x + " " + headY);
+		//this.parent.image(this.img, midX, midY);
+		this.parent.image(this.img, midX - img.width/2, midY - img.height/2);
 		this.parent.noTint();
 		while(diff > 0){
 			forward();
